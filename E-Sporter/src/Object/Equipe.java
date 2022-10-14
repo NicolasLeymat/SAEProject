9ba84bb1f3767c34097fe4c31d3626445426cd37
xavier cimber <xavier.cimber@etu.iut-tlse3.fr>
2022-10-14 14:55:27 +0200

    Update Equipe.java

--- E-Sporter/src/Object/Equipe.java
+++ E-Sporter/src/Object/Equipe.java
@@ -63,4 +63,36 @@ public class Equipe {
 		this.points = points;
 	}
 	
+	public int getLastId(Connection connex) {
+		java.sql.Statement st = null;
+		ResultSet rs;
+		int r = 0;
+		try {
+			st = connex.createStatement();
+			rs = st.executeQuery("Select id_equipes as id from equipe");
+			while (rs.next()) {
+				r = rs.getInt("id");
+			}
+		} catch (SQLException ee) {
+			ee.printStackTrace();
+		}
+		return r;
+	}
+
+	public void enregistrerEquipe(Connection connex, String nom, int nombreJoueur, int points, Scanner e) {
+		PreparedStatement pst;
+		int lastId = this.getLastId(connex);
+		try {
+			pst = connex.prepareStatement("insert into equipe values(?,?,?,?)");
+			pst.setInt(1, lastId+1);
+			pst.setString(2, nom);
+			pst.setString(3, nombreJoueur);
+			pst.setString(4, points);
+			pst.executeUpdate();
+		} catch (SQLException e1) {
+			
+			e1.printStackTrace();
+		}
+
+	}
 }
