a1e8b13db5fc701344a57723b50715d2aa3117e7
PacuraruDavid <pacurarudavidmihai@gmail.com>
2022-10-24 10:26:23 +0200

    Update Equipe.java

--- E-Sporter/src/Object/Equipe.java
+++ E-Sporter/src/Object/Equipe.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+//Classe qui défini les fonctions d'une équipe
 public class Equipe {
 
 
@@ -16,6 +17,7 @@ public class Equipe {
 	private Jeu jeu;
 	private List<Joueur> listeJoueurs;
 
+	//Constructeur de la classe "Equipe"
 	public Equipe(String nom, int points, Ecurie ecurie, Jeu jeu) {
 		this.nom = nom;
 		this.points = points;
@@ -24,38 +26,47 @@ public class Equipe {
 		this.listeJoueurs = new ArrayList<Joueur>();
 	}
 	
+	//Fonction qui permet de récuperer l'écurie d'une équipe
 	public Ecurie getEcurie() {
 		return ecurie;
 	}
-
+	
+	//Fonction qui permet de changer le nom de l'écurie de l'équipe
 	public void setEcurie(Ecurie ecurie) {
 		this.ecurie = ecurie;
 	}
 
+	//Fonction qui permet de retourner le jeu auquel une équipe joue
 	public Jeu getJeu() {
 		return jeu;
 	}
 
+	//Fonction qui permet de changer le nom du jeu auquel une équipe joue
 	public void setJeu(Jeu jeu) {
 		this.jeu = jeu;
 	}
-
+	
+	//Fonction qui permet de récuperer le nom d'une équipe
 	public String getNom() {
 		return nom;
 	}
-
+	
+	//Fonction qui permet de changer le nom d'une équipe
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+	//Fonction qui permet de récuperer les points gagnés par une équipe
 	public int getPoints() {
 		return points;
 	}
 
+	//Fonction qui permet de changer les points d'une équipe
 	public void setPoints(int points) {
 		this.points = points;
 	}
 	
+	//Fonction qui permet de récuperer le dernier identifiant d'une équipe
 	public int getLastId(Connection connex) {
 		java.sql.Statement st = null;
 		ResultSet rs;
@@ -72,10 +83,12 @@ public class Equipe {
 		return r;
 	}
 	
+	//Fonction qui permet d'ajouter un joueur à une équipe
 	public void addJoueur(Joueur joueur) {
 		this.listeJoueurs.add(joueur);
 	}
 
+	//Fonction qui permet d'enregistrer une équipe dans la base de données
 	public int enregistrerEquipe(Connection connex) throws Exception {
 		PreparedStatement pst;
 		int lastId = this.getLastId(connex);
