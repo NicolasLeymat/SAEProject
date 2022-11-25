ec158d5435197bc6b814cf5fb634c9dbe54ea2b7
NicolasLeymat <74655132+NicolasLeymat@users.noreply.github.com>
2022-11-25 10:33:02 +0100

    Update ModeleESporter.java

--- E-Sporter/src/controleur/ModeleESporter.java
+++ E-Sporter/src/controleur/ModeleESporter.java
@@ -1,6 +1,7 @@
 package controleur;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,12 +13,32 @@ public class ModeleESporter {
 
 	public static List<Equipe> resultatRechercheEquipes;
 	public static List<Ecurie> resultatRechercheEcuries;
+	public static List<Equipe> allEquipe;
+	public static List<Ecurie> allEcurie;
 	private static Connection connx;
 	
 	public ModeleESporter() {
 		resultatRechercheEcuries = new LinkedList<>();
 		resultatRechercheEquipes = new LinkedList<>();
 		connx = Connexion.connexion();
+	}
+	
+	public static void getAll() {
+		allEcurie = Ecurie.getAllEcuries(connx);
+		allEquipe = Equipe.getAllEquipes(connx);
+		try {
+			connx.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Object> recherche(String prompt){
+		for(Equipe e : allEquipe) {
+			
+		}
+		return null;
 	}
 	
 	public List<Equipe> getRecherche(String prompt){
