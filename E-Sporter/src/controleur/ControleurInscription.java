9b683a2d5c0b656d75cd3466a6c35a5fb869eef8
PacuraruDavid <pacurarudavidmihai@gmail.com>
2023-01-11 12:16:10 +0100

    Font changed and controleurInsc created

---
+++ E-Sporter/src/controleur/ControleurInscription.java
@@ -0,0 +1,14 @@
+package controleur;
+
+import IHM.tournois.InscriptionTournois;
+
+public class ControleurInscription {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
