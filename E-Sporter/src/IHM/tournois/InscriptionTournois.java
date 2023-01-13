2d347420e3a82427f8934878a9920ec6b725f17d
PacuraruDavid <pacurarudavidmihai@gmail.com>
2023-01-13 14:52:16 +0100

    Update InscriptionTournois

--- E-Sporter/src/IHM/tournois/InscriptionTournois.java
+++ E-Sporter/src/IHM/tournois/InscriptionTournois.java
@@ -37,17 +37,17 @@ public class InscriptionTournois extends JFrame {
 
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{54, 197, 0, 69, 29, 0, 0, 0, 0, 0, 43, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 37, 0, 0, 0, 0, 0, 46, 23, 41, 21, 51, 50, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{51, 170, 0, 69, 11, 175, 50, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 37, 0, 0, 0, 0, 46, 23, 63, 21, 51, 50, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JLabel lblNewLabel = new JLabel("Titre Tournois");
 		lblNewLabel.setFont(ModeleESporter.FONT_LARGE);
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridwidth = 2;
+		gbc_lblNewLabel.gridwidth = 3;
 		gbc_lblNewLabel.gridx = 2;
 		gbc_lblNewLabel.gridy = 1;
 		contentPane.add(lblNewLabel, gbc_lblNewLabel);
@@ -60,18 +60,17 @@ public class InscriptionTournois extends JFrame {
 		gbc_lblNewLabel_1.gridwidth = 2;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.gridx = 1;
-		gbc_lblNewLabel_1.gridy = 3;
+		gbc_lblNewLabel_1.gridy = 2;
 		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Sélectionnez une équipe ");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel lblNewLabel_2 = new JLabel("Equipes tournois");
 		lblNewLabel_2.setFont(ModeleESporter.FONT_MEDIUM);
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.gridwidth = 2;
 		gbc_lblNewLabel_2.gridheight = 2;
-		gbc_lblNewLabel_2.gridwidth = 7;
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_2.gridx = 4;
-		gbc_lblNewLabel_2.gridy = 3;
+		gbc_lblNewLabel_2.gridy = 2;
 		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -81,34 +80,36 @@ public class InscriptionTournois extends JFrame {
 		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 1;
-		gbc_scrollPane.gridy = 5;
+		gbc_scrollPane.gridy = 4;
 		contentPane.add(scrollPane, gbc_scrollPane);
 		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.gridwidth = 3;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 7;
-		gbc_comboBox.gridy = 7;
-		contentPane.add(comboBox, gbc_comboBox);
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.gridheight = 5;
+		gbc_scrollPane_1.gridwidth = 2;
+		gbc_scrollPane_1.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 4;
+		gbc_scrollPane_1.gridy = 4;
+		contentPane.add(scrollPane_1, gbc_scrollPane_1);
 		
-		JButton btnSupprimer = new JButton("Supprimer");
+		JButton btnSupprimer = new JButton("Ajouter");
 		btnSupprimer.setFont(ModeleESporter.FONT_SMALL);
 		GridBagConstraints gbc_btnSupprimer = new GridBagConstraints();
 		gbc_btnSupprimer.gridwidth = 2;
 		gbc_btnSupprimer.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSupprimer.gridx = 1;
-		gbc_btnSupprimer.gridy = 11;
+		gbc_btnSupprimer.gridy = 10;
 		contentPane.add(btnSupprimer, gbc_btnSupprimer);
 		
-		JButton btnNewButton = new JButton("Ajouter");
-		btnNewButton.setFont(ModeleESporter.FONT_SMALL);
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 8;
-		gbc_btnNewButton.gridy = 9;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
+		JButton btnSupprimer_1 = new JButton("Supprimer");
+		btnSupprimer_1.setFont(ModeleESporter.FONT_SMALL);
+		GridBagConstraints gbc_btnSupprimer_1 = new GridBagConstraints();
+		gbc_btnSupprimer_1.gridwidth = 2;
+		gbc_btnSupprimer_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSupprimer_1.gridx = 4;
+		gbc_btnSupprimer_1.gridy = 10;
+		contentPane.add(btnSupprimer_1, gbc_btnSupprimer_1);
 	}
 
 }
