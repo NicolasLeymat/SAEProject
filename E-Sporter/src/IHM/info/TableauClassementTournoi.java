131435215bca35a50b958fe0ff13e9a8d0aa3981
Louis <camborieuxl@gmail.com>
2023-02-17 15:45:36 +0100

    Corrections SeeInfoFrame TableauClassementTournoi Suppression code mort + ajout de commentaires

--- E-Sporter/src/IHM/info/TableauClassementTournoi.java
+++ E-Sporter/src/IHM/info/TableauClassementTournoi.java
@@ -4,17 +4,16 @@ import javax.swing.table.AbstractTableModel;
 import Object.Tournoi;
 import Object.Equipe;
 
-import java.util.Arrays;
+
 import java.util.Map;
 
 public class TableauClassementTournoi extends AbstractTableModel {
     private String[] columnNames = {"#", "Equipe", "V","D"};
     private Tournoi tournoi;
     private Equipe[] equipes;
-
     private Map<Equipe,Integer[]> victoires;
-
     public TableauClassementTournoi(Tournoi tournoi) {
+        //Recupere les equipes triés par leur classement
         try {
             equipes = tournoi.getPhaseElim().getClassement();
         }
@@ -22,10 +21,10 @@ public class TableauClassementTournoi extends AbstractTableModel {
             e.printStackTrace();
             return;
         }
+        //Recupere les victoires
         victoires = tournoi.getVictoires();
         this.tournoi = tournoi;
     }
-
     @Override
     public int getRowCount() {
         try {
@@ -36,21 +35,27 @@ public class TableauClassementTournoi extends AbstractTableModel {
         }
 
     }
-
     @Override
     public int getColumnCount() {
         return columnNames.length;
     }
-
     public String getColumnName(int col) {
         return columnNames[col];
     }
 
+    /**
+     * Affiche les infos à partir d'une ligne et d'une colonne
+     * @param rowIndex        la ligne
+     * @param columnIndex     la colonne
+     * @return
+     */
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
+        //Compare les ID car deux objets differents sont crées
         Equipe equipet = victoires.keySet().stream().filter((e) -> e.getId() == equipes[rowIndex].getId()).findFirst().orElse(null);
         switch (columnIndex) {
             case 0:
+                //Affiche le classement
                 return rowIndex +1;
             case 1:
                 return equipes[rowIndex].getNom();
