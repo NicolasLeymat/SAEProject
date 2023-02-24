a6a75adfefcd79a35dd3471665a82697ba300360
PacuraruDavid <pacurarudavidmihai@gmail.com>
2023-02-24 15:12:40 +0100

    ChampPanel IHM update
    Added an "Annuler" button and resized the list of the global leaderboard

--- E-Sporter/src/IHM/champ/ChampFrame.java
+++ E-Sporter/src/IHM/champ/ChampFrame.java
@@ -9,7 +9,7 @@ import java.awt.GridBagConstraints;
 public class ChampFrame extends JFrame {
 
 
-	private final int WIDTH = 450;
+	private final int WIDTH = 500;
 	private final int HEIGHT = 500;
 	private ChampPanel contentPane;
 	private String mode;
@@ -20,20 +20,21 @@ public class ChampFrame extends JFrame {
 	public ChampFrame() {
 		this.setResizable(false);
 		this.setName("Classement Mondiaux");
-		this.setMinimumSize(new Dimension(WIDTH, HEIGHT-200));
+		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0};
-		gridBagLayout.rowHeights = new int[]{0};
+		gridBagLayout.columnWidths = new int[]{1};
+		gridBagLayout.rowHeights = new int[]{1};
 		gridBagLayout.columnWeights = new double[]{1.0};
 		gridBagLayout.rowWeights = new double[]{1.0};
 		getContentPane().setLayout(gridBagLayout);
 
 		contentPane = new ChampPanel();
-		GridBagConstraints gbc_contentPane = new GridBagConstraints();
+		GridBagConstraints gbc_contentPane = new GridBagConstraints();		
 		gbc_contentPane.gridy = 0;
 		gbc_contentPane.gridx = 0;
 		gbc_contentPane.fill = GridBagConstraints.BOTH;
+		
 		getContentPane().add(contentPane, gbc_contentPane);		
 	}
 
--- E-Sporter/src/IHM/champ/ChampPanel.java
+++ E-Sporter/src/IHM/champ/ChampPanel.java
@@ -11,21 +11,24 @@ import javax.swing.ScrollPaneConstants;
 import Object.Equipe;
 import controleur.ControleurFilter;
 import controleur.ControleurJList;
+import controleur.ControleurModif;
 import controleur.ModeleESporter;
 
-import java.awt.Dimension;
+import java.awt.Color;
+
+import java.awt.Font;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JComboBox;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+
 
 public class ChampPanel extends JPanel {
 
-	//Class Variable
-	private final int WIDTH = 450;
-	private final int HEIGHT = 500;
-	private final int MAIN_FONT_SIZE = 17;
-	private String mode;
-	
+	private Equipe e;
 	private JComboBox<String> filterMode;
 	
 	private static JList<Equipe> listEquipe = new JList<>();
@@ -33,35 +36,66 @@ public class ChampPanel extends JPanel {
 	
 	
 	public ChampPanel() {
-		ControleurJList controleurEquipe = new ControleurJList();
+		ControleurJList controleurEquipe = new ControleurJList();	
+		ControleurModif controleurModif = new ControleurModif(e, this); 
 		ChampPanel.changeModelResultChamp(Equipe.getClassementByGame(0));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{350, 0};
+		gridBagLayout.rowHeights = new int[]{350, 50, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		ControleurFilter<String> controleurMode = new ControleurFilter<>("Championnat");
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		add(panel, gbc_panel);
 		listEquipe.addMouseListener(controleurEquipe);
 		listEquipe.setModel(modelEquipe);
 		JScrollPane teamPanel = new JScrollPane(listEquipe);
+		teamPanel.setBounds(90, 50, 300, 300);
+		panel.add(teamPanel);
 		teamPanel.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		teamPanel.setSize(400, 625);
-		teamPanel.setMaximumSize(new Dimension(400, 625));
-		teamPanel.setMinimumSize(new Dimension(400, 625));
-		add(teamPanel);
 		
 		
 		
 		JPanel filterTeamPanel = new JPanel();
 		teamPanel.setColumnHeaderView(filterTeamPanel);
-		
-		ControleurFilter<String> controleurMode = new ControleurFilter<>("Championnat");
 		filterMode = new JComboBox<>();
-		for(String s : ModeleESporter.getAllModeName()){
-			filterMode.addItem(s);
-		}
 		filterMode.setFont(ModeleESporter.FONT_MEDIUM);
 		filterMode.addActionListener(controleurMode);
 		filterTeamPanel.add(filterMode);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		add(panel_1, gbc_panel_1);
+		
+		
+		
+		JButton btnAnnuler = new JButton("Annuler");
+		btnAnnuler.setForeground(new Color(255, 0, 0));
+		btnAnnuler.setFont(new Font("Berlin Sans FB", Font.PLAIN, ModeleESporter.FONT_SIZE_LARGE));
+		btnAnnuler.setBounds(10, 10, 200, 50);
+		btnAnnuler.addActionListener(controleurModif);
+		panel_1.add(btnAnnuler); 
+		 
+		
+		for(String s : ModeleESporter.getAllModeName()){
+			filterMode.addItem(s);
+		}
 	}
 	
 	public static void changeModelResultChamp(List<Equipe> l) {
 		modelEquipe.clear();
 		modelEquipe.addAll(l);
 	}
-
 }
